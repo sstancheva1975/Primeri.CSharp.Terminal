b4ce1cc9a26f627bb34941864dd3f5a95202bb8b
Stefka Stancheva <sstancheva1975@abv.bg>
2017-01-11 14:35:12 +0200

    Add files via upload

---
+++ Length.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace H02
+{
+    class Length
+    {
+        static void Main()
+        {
+            /*Да се въведат от командния ред 1 число или цифра и 1 дума/стринг.*/
+            int number = 0;
+            string word = "";
+            Console.Write("Въведете число: ");
+            number = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Въведете дума: ");
+            word = Console.ReadLine();
+            /* След това да изведе дължината на получения стринг при конкатенация,
+             * тоест броя на символите в новополучения стринг при конкатенация.
+             * (Hint: Трябва да преобразувате int към String и след това да конкатениратe.)
+             */
+           
+            Console.Write("Броят на символите в новополучения стринг при конкатенацията на числото и думата е: ");
+            Console.WriteLine(number.ToString().Length + word.Length);
+        }
+    }
+}
---
+++ TypeCasting.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace H01
+{
+    class TypeCasting
+    {
+        static void Main()
+        {
+            /*Зад.1
+             * Декларирайте две променливи от тип string и им присвоете
+             * стойности "Hello" и "World".*/
+            string first= "Hello";
+            string second = "World!";
+            /*Декларирайте променлива от тип object и 
+             *й присвоете стойността на конкатенацията
+             *на двете променливи от тип string
+             *(не изпускайте интервала по средата).*/
+            object obj = first + " " + second;
+            Console.WriteLine(obj);
+            /*Декларирайте трета променлива от тип string
+             *и я инициализирайте със стойността на
+             *променливата от тип object.
+            *( Hint: Трябва да използвате type casting.)
+             */
+            string third;
+            third = (string)obj;
+            Console.WriteLine(third);
+
+            
+          }
+
+
+      }
+}
